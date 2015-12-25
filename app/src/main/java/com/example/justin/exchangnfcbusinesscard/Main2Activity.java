5fdd2802e7bdfc0ae6755563159df1ba154990cb
Chang-Qi Zhang <justin840727@gmail.com>
2015-12-25 16:56:41 +0800

    add my sourcecode

---
+++ app/src/main/java/com/example/justin/exchangnfcbusinesscard/Main2Activity.java
@@ -0,0 +1,172 @@
+package com.example.justin.exchangnfcbusinesscard;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+
+public class Main2Activity extends AppCompatActivity {
+    private DisplayMetrics mPhone;
+    private final static int CAMERA = 66 ;
+    private final static int PHOTO = 99 ;
+
+    ImageButton btn;
+    Button savebtn;
+    SharedPreferences pre;
+    SharedPreferences.Editor preEdit;
+    EditText name,job,cellphone,email,company,phone,address;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
+            //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            //setSupportActionBar(toolbar);
+        //讀取手機解析度
+        mPhone = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(mPhone);
+
+            btn = (ImageButton) findViewById(R.id.imageButton);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Main2Activity.this);
+                    builder.setTitle("選擇頭像");
+                    builder.setMessage("請選擇來源");
+                    builder.setPositiveButton("照相", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick (DialogInterface dialog,int which){//到照相畫面
+                                Toast.makeText(getApplicationContext(), "照相", Toast.LENGTH_SHORT).show();
+                                /*開啟相機功能，並將拍照後的圖片存入SD卡相片集內，須由startActivityForResult且
+                                帶入 requestCode進行呼叫，原因為拍照完畢後返回程式後則呼叫onActivityResult*/
+                                ContentValues value = new ContentValues();
+                                value.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                                Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                        value);
+                                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri.getPath());
+                                startActivityForResult(intent, CAMERA);
+                            /*private Camera.PictureCallback jpeg = new Camera.PictureCallback() {//執行照相功能，傳入JPEG資料
+                                public void onPictureTaken(byte[] bytes, Camera camera) {
+                                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                    try {//設定要儲存的圖檔位置與名稱
+                                        File imgFile = new File("/sdcard/myCamera_" + new java.util.Data().getTime + ".jpg");
+                                        //設定輸出資料流
+                                        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(imgFile));
+                                        //設定檔案格式與圖檔品質
+                                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                                        //寫出檔案
+                                        out.flush();
+                                        out.close();
+                                        out = null;
+                                        //進入預覽
+                                        bfCamera.startPreview();
+                                    } catch (Exception e) {
+                                        Log.e("camera", "take photo error:" + e);
+                                    }
+                                }
+                            };*/
+                            }
+                        }
+
+                    );
+                    builder.setNegativeButton("相簿", new DialogInterface.OnClickListener()
+
+                            {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {//到相簿畫面
+                                    Toast.makeText(getApplicationContext(), "相簿", Toast.LENGTH_SHORT).show();
+                                    //this.requestWindowFeature(Window.FEATURE_NO_TITLE);//設定使用全螢幕顯示，要置於setContentView之前
+                                    /*開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因
+                                    為點選相片後返回程式呼叫onActivityResult*/
+                                    Intent intent = new Intent();
+                                    intent.setType("image/*");
+                                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                                    startActivityForResult(intent, PHOTO);
+                                }
+                            }
+
+                    );
+                    builder.show();
+                }
+            });
+
+            savebtn = (Button) findViewById(R.id.Savebtn);
+            savebtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    name = (EditText) findViewById(R.id.editText1);
+                    job = (EditText) findViewById(R.id.editText2);
+                    cellphone = (EditText) findViewById(R.id.editText3);
+                    email = (EditText) findViewById(R.id.editText4);
+                    company = (EditText) findViewById(R.id.editText5);
+                    phone = (EditText) findViewById(R.id.editText6);
+                    address = (EditText) findViewById(R.id.editText7);
+                    String temp1 = name.getText().toString();
+                    String temp2 = job.getText().toString();
+                    String temp3 = cellphone.getText().toString();
+                    String temp4 = email.getText().toString();
+                    String temp5 = company.getText().toString();
+                    String temp6 = phone.getText().toString();
+                    String temp7 = address.getText().toString();
+                    Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                    intent.putExtra("REQ1", temp1);
+                    intent.putExtra("REQ2", temp2);
+                    intent.putExtra("REQ3", temp3);
+                    intent.putExtra("REQ4", temp4);
+                    intent.putExtra("REQ5", temp5);
+                    intent.putExtra("REQ6", temp6);
+                    intent.putExtra("REQ7", temp7);
+                    setResult(1010, intent);
+                    startActivity(intent);
+                    Main2Activity.this.finish();
+                    // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+                    //fab.setOnClickListener(new View.OnClickListener() {
+                    // @Override
+                    //public void onClick(View v) {
+                    // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    //   .setAction("Action", null).show();
+                }
+            });
+    }
+    //拍照完畢或選取圖片後呼叫此函式
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data)
+    {
+        //藉由requestCode判斷是否為開啟相機或開啟相簿而呼叫的，且data不為null
+        if ((requestCode == CAMERA || requestCode == PHOTO ) && data != null)
+        {
+            //取得照片路徑uri
+            Uri uri = data.getData();
+            ContentResolver cr = this.getContentResolver();
+
+            try
+            {
+                //讀取照片，型態為Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+
+            }
+            catch (FileNotFoundException e)
+            {
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
---
+++ app/src/main/java/com/example/justin/exchangnfcbusinesscard/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.justin.exchangnfcbusinesscard;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    SharedPreferences pre;
+    SharedPreferences.Editor preEdit;
+    ImageView img;
+    TextView name,job,cellphone,email,company,phone,address;
+    Button btn1,btn2,btn3,btn4,btn5,btn6;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        }
+
+
+
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+   /*         @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+      /*  DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);*/
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {//選擇查看名片夾
+            btn1=(Button)findViewById(R.id.nav_camera);
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivityForResult(intent, 0);
+                }
+            });
+        } else if (id == R.id.nav_gallery) {
+           btn2=(Button)findViewById(R.id.nav_gallery);
+           btn2.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                   startActivityForResult(intent, 1);
+               }
+           });
+        } else if (id == R.id.nav_slideshow) {
+            btn3=(Button)findViewById(R.id.nav_slideshow);
+            btn3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivityForResult(intent, 2);
+                }
+            });
+        } else if (id == R.id.nav_manage) {
+            btn4=(Button)findViewById(R.id.nav_gallery);
+            btn4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivityForResult(intent, 3);
+                }
+            });
+        } else if (id == R.id.nav_share) {
+            btn5=(Button)findViewById(R.id.nav_gallery);
+            btn5.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivityForResult(intent, 4);
+                }
+            });
+        } else if (id == R.id.nav_send) {
+            btn6=(Button)findViewById(R.id.nav_gallery);
+            btn6.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    startActivityForResult(intent, 5);
+                }
+            });
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == 101) {
+                Bundle b = data.getExtras();
+                //在TextView顯示對應資料
+
+            }
+        }
+        if (requestCode == 1) {
+            if (resultCode == 1010) {
+                Bundle b = data.getExtras();
+                //在TextView顯示對應資料
+                img=(ImageView) findViewById(R.id.imageView);
+                name = (TextView) findViewById(R.id.Name);
+                job = (TextView) findViewById(R.id.Job);
+                cellphone = (TextView) findViewById(R.id.Cellphone);
+                email = (TextView) findViewById(R.id.Email);
+                company = (TextView) findViewById(R.id.Company);
+                phone = (TextView) findViewById(R.id.Phone);
+                address = (TextView) findViewById(R.id.Address);
+                name.setText(b.getString("REQ1"));
+                job.setText(b.getString("REQ2"));
+                cellphone.setText(b.getString("REQ3"));
+                email.setText(b.getString("REQ4"));
+                company.setText(b.getString("REQ5"));
+                phone.setText(b.getString("REQ6"));
+                address.setText(b.getString("REQ7"));
+            }
+        }
+    }
+}
