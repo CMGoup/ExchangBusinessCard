5aec9f3adc538f844cbf85502d772c3b5d58a51f
Chang-Qi Zhang <justin840727@gmail.com>
2015-12-26 23:57:25 +0800

    Add DataBase class

---
+++ app/src/main/java/com/example/justin/exchangnfcbusinesscard/SqlDataCtrl.java
@@ -0,0 +1,10 @@
+package com.example.justin.exchangnfcbusinesscard;
+
+/**
+ * Created by Justin on 2015/12/26.
+ */
+public class SqlDataCtrl {
+    public static final String TABLE_NAME = "item";
+    public static final String KEY_ID = "_id";
+
+}
